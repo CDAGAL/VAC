d8a7986a81da1ed54c3d3ae99fc353ae564f83fd
MRX <48726203+5korpi0n@users.noreply.github.com>
2020-01-02 10:50:55 -0300

    creacion de VAC
    importo carpeta del proyecto

---
+++ vac/src/main/java/com/cdainfo/vac/VacApplication.java
@@ -0,0 +1,13 @@
+package com.cdainfo.vac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacApplication.class, args);
+	}
+
+}
